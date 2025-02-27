68d81a602543118aa2f7f3f6eb1d8ffe9e803d83
nanigurijala <gurijalananii@gmail.com>
2025-02-27 16:29:47 +0530

    Delete exproject directory

--- exproject/exapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from exapp.models import demoModel
-# Register your models here.
-
-admin.site.register(demoModel)
--- exproject/exapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class demoModel(models.Model):
-    name=models.CharField(max_length=50)
-    address=models.CharField(max_length=100)
-    phone=models.IntegerField()
-    email=models.EmailField()
